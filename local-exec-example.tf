e770b17714b7fe378cf0e421260ad64254aaab3e
Azam-JM <155773000+Azam-JM@users.noreply.github.com>
2024-09-22 19:45:37 +0100

    Create local-exec-example.tf
    Local exec provisioner example

---
+++ local-exec-example.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "eu-west-1"
+}
+
+resource "aws_instance" "local" {
+    instance_type = "t2.micro"
+    ami           = "ami-0fed63ea358539e44"
+    tags = {
+        Name = "local-ec2"
+    }
+
+    provisioner "local-exec" {
+        command = "echo Instance Id: ${self.id} > instance_id.txt"      
+    }
+}
+
